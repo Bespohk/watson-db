329731aa7de21c588407ea16064b99683c520d5b
Simon Coulton <simon.coulton@gmail.com>
2016-03-11 11:03:10 +1100

    Paginator now returns Page objects with query strings

--- tests/watson/db/test_utils.py
+++ tests/watson/db/test_utils.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from pytest import raises
-from sqlalchemy.orm import exc
 from watson.db import utils
 from tests.watson.db.support import session, Test
 
@@ -9,8 +7,9 @@ class TestPaginator(object):
 
     def test_create_paginator(self):
         paginator = utils.Pagination(session.query(Test))
+        expects = '<watson.db.utils.Pagination page:1 limit:20 total:6 pages:1>'
         assert paginator
-        assert repr(paginator) == '<watson.db.utils.Pagination page:1 limit:20 total:6 pages:1>'
+        assert repr(paginator) == expects
 
     def test_iter_pages(self):
         paginator = utils.Pagination(session.query(Test), page=2, limit=2)
@@ -30,3 +29,32 @@ class TestPaginator(object):
     def test_no_results(self):
         paginator = utils.Pagination(session.query(Test).filter(Test.value == 10), page=2)
         assert not paginator.has_next
+
+    def test_next_previous_page(self):
+        paginator = utils.Pagination(session.query(Test), page=2, limit=2)
+        assert paginator.next.id == 3
+        assert paginator.previous.id == 1
+
+        paginator = utils.Pagination(session.query(Test), page=1, limit=2)
+        assert not paginator.previous
+        paginator = utils.Pagination(session.query(Test), page=3, limit=2)
+        assert not paginator.next
+
+    def test_page_query_string(self):
+        paginator = utils.Pagination(session.query(Test), page=2, limit=2)
+        assert str(paginator.next) == '?page=3'
+        assert str(paginator.previous) == '?page=1'
+
+    def test_page_query_string_append(self):
+        paginator = utils.Pagination(session.query(Test), page=2, limit=2)
+        paginator.next.append(test='test')
+        next_query = str(paginator.next)
+        assert 'test=test' in next_query
+        assert 'page=3' in next_query
+        paginator.next.append(anothervalue='value')
+        next_query = str(paginator.next)
+        assert 'anothervalue=value' in next_query
+        assert 'test=test' in next_query
+        paginator.previous.append(test='test')
+        prev_query = str(paginator.previous)
+        assert 'page=1' in prev_query
--- watson/db/utils.py
+++ watson/db/utils.py
@@ -7,6 +7,40 @@ def _table_attr(obj, attr):
     return '{}.{}'.format(obj.__tablename__, attr)
 
 
+class Page(object):
+    """A single page object that is returned from the paginator.
+
+    Provides the ability to automatically generate a query string.
+    """
+    id = None
+    query_string_key = 'page'
+    __parts = None
+
+    def __init__(self, id):
+        self.id = id
+        self._parts[self.query_string_key] = self.id
+
+    @property
+    def query_string(self):
+        parts = ['{}={}'.format(key, value)
+                 for key, value in self._parts.items()]
+        return '?{}'.format('&amp;'.join(parts))
+
+    def append(self, **kwargs):
+        self._parts.update(**kwargs)
+        return self
+
+    # internal
+    @property
+    def _parts(self):
+        if not self.__parts:
+            self.__parts = {}
+        return self.__parts
+
+    def __str__(self):
+        return self.query_string
+
+
 class Pagination(object):
     """Provides simple pagination for query results.
 
@@ -31,9 +65,9 @@ class Pagination(object):
         <div class="pagination">
         {% for page in paginator.iter_pages() %}
             {% if page == paginator.page %}
-            <a href="#" class="current">{{ page }}</a>
+            <a href="{{ page }}" class="current">{{ page.id }}</a>
             {% else %}
-            <a href="#">{{ page }}</a>
+            <a href="{{ page }}">{{ page.id }}</a>
             {% endif %}
         {% endfor %}
         </div>
@@ -43,6 +77,8 @@ class Pagination(object):
     limit = None
     total = None
     items = None
+    _cached_next = None
+    _cached_previous = None
 
     def __init__(self, query, page=1, limit=20):
         """Initialize the paginator and set some default values.
@@ -63,6 +99,20 @@ class Pagination(object):
         return self.page > 1
 
     @property
+    def previous(self):
+        """Return the previous page object if the page exists.
+
+        Returns:
+            Page
+        """
+        if self.has_previous:
+            previous_id = self.page - 1
+            if self._cached_previous and self._cached_previous.id == previous_id:
+                return self._cached_previous
+            self._cached_previous = Page(previous_id)
+            return self._cached_previous
+
+    @property
     def has_next(self):
         """Return whether or not there are more pages from the currently
         displayed page.
@@ -71,6 +121,20 @@ class Pagination(object):
             boolean
         """
         return self.page < self.pages
+
+    @property
+    def next(self):
+        """Return the next page object if another page exists.
+
+        Returns:
+            Page
+        """
+        if self.has_next:
+            next_id = self.page + 1
+            if self._cached_next and self._cached_next.id == next_id:
+                return self._cached_next
+            self._cached_next = Page(next_id)
+            return self._cached_next
 
     @property
     def pages(self):
@@ -95,7 +159,7 @@ class Pagination(object):
             {% for page in paginator.iter_pages() %}{% endfor %}
         """
         for num in range(1, self.pages + 1):
-            yield num
+            yield Page(num)
 
     # Internals
 
